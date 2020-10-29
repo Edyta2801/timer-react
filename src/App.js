0ac464c0f1ec0c4c3cc4c30163c287ed31fb8de2
Edyta2801 <edyta.szarowska@interia.pl>
2020-10-29 10:50:21 +0100

    add class component and umounting

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class TimerClass extends Component {
+  state={
+    counter:0
+  };
+   render(){
+     const {counter}= this.state;
+     return(<h2>{counter}</h2>);
+   };
+   componentDidMount(){
+     this.counterRef=setInterval(()=>{
+       this.setState({counter:this.state.counter +1});
+     },1000)
+   }
+   componentWillUnmount(){
+     clearInterval(this.counterRef);
+   }
+
 }
 
-export default App;
+export default function App(){
+  return(
+    <div className="App">
+      <TimerClass/>
+    </div>
+  )
+}
