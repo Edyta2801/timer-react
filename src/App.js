c52977d47ca49c6e8d7e27bac7cc7da99d4f7079
Edyta2801 <edyta.szarowska@interia.pl>
2020-10-29 11:49:13 +0100

    finction component with useState and useEffect

--- src/App.js
+++ src/App.js
@@ -1,29 +1,46 @@
-import React, {Component} from 'react';
+import React, { useState, useEffect, Component } from 'react';
 import './App.css';
 
 class TimerClass extends Component {
-  state={
-    counter:0
+  state = {
+    counter: 0
   };
-   render(){
-     const {counter}= this.state;
-     return(<h2>{counter}</h2>);
-   };
-   componentDidMount(){
-     this.counterRef=setInterval(()=>{
-       this.setState({counter:this.state.counter +1});
-     },1000)
-   }
-   componentWillUnmount(){
-     clearInterval(this.counterRef);
-   }
+  render() {
+    const { counter } = this.state;
+    return (<h2>{counter}</h2>);
+  };
+  componentDidMount() {
+    this.counterRef = setInterval(() => {
+      this.setState({ counter: this.state.counter + 1 });
+    }, 1000)
+  }
+  componentWillUnmount() {
+    clearInterval(this.counterRef);
+  }
 
 }
 
-export default function App(){
-  return(
+function TimerFn() {
+  const[ counter, setCounter ] = useState(0);
+
+
+  useEffect(() => {
+    const counterRef = setInterval(() => {
+      setCounter(counter=>counter + 1);
+    }, 1000);
+    return () => clearInterval(counterRef);
+  }, []);
+  return <h2>{counter}</h2>;
+
+}
+
+
+
+export default function App() {
+  return (
     <div className="App">
-      <TimerClass/>
+      <TimerClass />
+      <TimerFn />
     </div>
   )
 }
